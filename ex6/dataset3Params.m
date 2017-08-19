b9fec86c8bf6f05709f5f8a74bc24a2a3b3fa11b
DSdatsme <you@example.com>
2017-08-19 21:03:07 +0530

    Parameters (C, sigma) for Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,8 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+min_error = Inf;
 
+multiplicative_steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+for _C_ = multiplicative_steps
+	for _sigma_ = multiplicative_steps
+		model = svmTrain(X, y, _C_, @(x1, x2) gaussianKernel(x1, x2, _sigma_));
+		
+		predictions = svmPredict(model, Xval);
+		predictionError = mean(double(predictions ~= yval));
+		
+		if predictionError < min_error
+			min_error = predictionError;
+			C = _C_;
+			sigma = _sigma_;
+		end
+	end	
+end
 
 
 
